1f6de782577379ee3b841806160c9076b6bfb222
Saliinger <alexisnoukan@gmail.com>
2026-06-24 12:44:24 +0200

    norm

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: alnoukan <alnoukan@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/24 02:10:18 by alnoukan          #+#    #+#             */
-/*   Updated: 2026/06/24 02:10:22 by alnoukan         ###   ########.fr       */
+/*   Updated: 2026/06/24 12:44:04 by alnoukan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,59 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+}
+
+void	ft_printnbr(int n)
+{
+	if (n < 10)
+		ft_putchar('0');
+	ft_putnbr(n);
+}
+
 void	ft_print_comb2(void)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 100)
+	{
+		j = 0;
+		while (j < 100)
+		{
+			ft_printnbr(i);
+			ft_putchar(' ');
+			ft_printnbr(j);
+			ft_putchar(',');
+			j++;
+		}
+		i++;
+	}
 }
+
+// int main(void)
+// {
+// 	ft_print_comb2();
+// 	return (0);
+// }
